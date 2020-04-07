c543facea74c05c3ca842d2849d5363fd4ef7de8
Curtis Chadwell <laurencechadwell@gmail.com>
2020-04-07 01:10:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,25 +1,19 @@
 class Artist 
-  @@all = []
+  
   attr_accessor :name
 
   def initialize(name)
     @name = name
-    @@all.push(self)
+    @song = []
   end
 
   def songs
-    @@all.select do |song|
-      song
-    end
+   @song
   end
   
-  def add_song(song)
-    
-  end
   
-  def self.all
-    @@all
-  end
+  
+  
   
   
 
