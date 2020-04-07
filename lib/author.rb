ebdf684fc8a1a33d8e8a3dbb9059db0afd051ba8
Curtis Chadwell <laurencechadwell@gmail.com>
2020-04-07 02:36:00 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,5 @@
 class Author 
-  
+  @@post_count = 0
   
   attr_accessor :name
   @@all = Array.new
@@ -25,5 +25,12 @@ class Author
   def add_post_by_title(title)
     post = Post.new(title)
     post.author = self
+    @@post_count += 1
   end
+  
+  def self.post_count 
+    @@post_count
+  end
+  
+  
 end
